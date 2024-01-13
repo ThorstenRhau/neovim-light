c96d08c71b6177566e9d31562376f52f231376e2
Thorsten Rhau <github@thorre.se>
2024-01-13 16:02:27 +0100

    Re-worked treesitter plugin configuration
    Added nvim-ts-autotag for automatic creation
    of tags in markup languages.
    Added keymapping for incremental_selection of code.
    Added a lot of pre-installed language parsers.

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    dependencies = "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter-context",
+        "windwp/nvim-ts-autotag",
+    },
     version = false,
     build = ":TSUpdate",
-    lazy = true,
     event = { "BufReadPost", "BufNewFile", "BufWritePre" },
     opts = function()
         require("nvim-treesitter.configs").setup({
@@ -11,6 +13,9 @@ return {
             sync_install = false,
             ignore_install = {},
             auto_install = true,
+            autotag = {
+                enable = true,
+            },
             highlight = {
                 enable = true,
                 additional_vim_regex_highlighting = false,
@@ -18,9 +23,27 @@ return {
             indent = {
                 enable = true,
             },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+            ---@diagnostic disable-next-line: unused-local
+            disable = function(lang, buf)
+                local max_filesize = 500 * 1024 -- 500 KB
+                local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                if ok and stats and stats.size > max_filesize then
+                    return true
+                end
+            end,
             ensure_installed = {
                 "bash",
                 "comment",
+                "css",
                 "diff",
                 "git_config",
                 "git_rebase",
@@ -29,16 +52,35 @@ return {
                 "gitignore",
                 "html",
                 "http",
+                "javascript",
+                "json",
+                "json5",
                 "jsonc",
                 "lua",
                 "luadoc",
-                "python",
+                "make",
                 "markdown",
                 "markdown_inline",
+                "passwd",
+                "pem",
+                "perl",
+                "php",
+                "phpdoc",
+                "properties",
+                "pymanifest",
+                "python",
                 "regex",
+                "requirements",
+                "ruby",
+                "scheme",
+                "sql",
+                "ssh_config",
+                "todotxt",
                 "toml",
+                "typescript",
                 "vim",
                 "vimdoc",
+                "xml",
                 "yaml",
             },
         })

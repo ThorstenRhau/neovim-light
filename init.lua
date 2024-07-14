a25b768d77a954862162a2bad450fc0a5bc9441c
Thorsten Rhau <github@thorre.se>
2024-07-14 16:59:58 +0200

    Removed custom performance settings

--- init.lua
+++ init.lua
@@ -33,28 +33,6 @@ require("lazy").setup({
         size = { width = 0.9, height = 0.9 },
         border = "rounded",
     },
-    performance = {
-        cache = {
-            enabled = true,
-        },
-        reset_packpath = true, -- reset the package path to improve startup time
-        rtp = {
-            reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
-            ---@type string[]
-            paths = {}, -- add any custom paths here that you want to includes in the rtp
-            ---@type string[] list any plugins you want to disable here
-            disabled_plugins = {
-                "gzip",
-                "matchit",
-                "matchparen",
-                "netrwPlugin",
-                "tarPlugin",
-                "tohtml",
-                "tutor",
-                "zipPlugin",
-            },
-        },
-    },
 })
 
 require("config.options")

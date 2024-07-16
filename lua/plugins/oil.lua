3062cbb4b65f00b3ac3b8160bf119045ecdd3d25
Thorsten Rhau <github@thorre.se>
2024-07-16 07:11:48 +0200

    New 'Oil' configuration

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,28 @@
 return {
     "stevearc/oil.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    lazy = true,
+    dependencies = {
+        "echasnovski/mini.icons",
+    },
     cmd = "Oil",
-    opts = {},
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        skip_confirm_for_simple_edits = false,
+        columns = {
+            "icon",
+        },
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, _)
+                return name == ".." or name == ".git"
+            end,
+        },
+        win_options = {
+            wrap = true,
+            cursorcolumn = true,
+        },
+        watch_for_changes = true,
+        use_default_keymaps = true,
+        show_hidden = true,
+    },
 }

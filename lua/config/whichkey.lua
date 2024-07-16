af1362a091fb02bd33d80a6fe1bcb7033a320fe3
Thorsten Rhau <github@thorre.se>
2024-07-16 05:59:04 +0200

    Updated keymappings in whichkey

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -13,12 +13,18 @@ wk.add({
     { "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil file manager" },
     { "<leader>p", "<cmd>Pounce<cr>", desc = "Pounce" },
     { "<leader>s", '<cmd> lua require("persistence").load() <cr>', desc = "Restore last session" },
-    { "<leader>t", "<cmd>Telescope<cr>", desc = "Telescope" },
+    { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal toggle" },
     { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Undo" },
+    --                                  ╭────────╮
+    --                                  │ Buffer │
+    --                                  ╰────────╯
     { "<leader>b", group = "Buffer" },
     { "<leader>bb", "<cmd>bprev<CR>", desc = "Previous" },
     { "<leader>be", "<cmd>Neotree buffers<CR>", desc = "Neotree buffers" },
     { "<leader>bl", "<cmd>ls<CR>", desc = "List" },
+    --                                   ╭──────╮
+    --                                   │ Find │
+    --                                   ╰──────╯
     { "<leader>f", group = "Find" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
@@ -30,16 +36,21 @@ wk.add({
     { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy find in buffer" },
     { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Old Yanks" },
     { "<leader>fz", "<cmd>Telescope spell_suggest<cr>", desc = "Spelling suggestions" },
+    --                                    ╭─────╮
+    --                                    │ Git │
+    --                                    ╰─────╯
     { "<leader>g", group = "Git" },
     { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Blame toggle" },
     { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Shows previous commits" },
     { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive Git" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
-    { "<leader>u", group = "User Interface" },
+    --                                 ╭───────────╮
+    --                                 │ Interface │
+    --                                 ╰───────────╯
+    { "<leader>u", group = "Interface" },
     { "<leader>uC", "<cmd>set colorcolumn=80<CR>", desc = "Colorcolumn at 80" },
     { "<leader>uh", "<cmd>set list!<CR>", desc = "Hidden Characters Toggle" },
     { "<leader>ul", "<cmd>set number!<cr>", desc = "Line number toggle" },
     { "<leader>ur", "<cmd>set relativenumber!<cr>", desc = "Relative line number toggle" },
-    { "<leader>ut", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal toggle" },
     { "<leader>uw", "<cmd>set wrap!<cr>", desc = "Wrap line toggle" },
 })

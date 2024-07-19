a93e408139107f172d54c606021afacc195810a1
Thorsten Rhau <github@thorre.se>
2024-07-19 08:41:34 +0200

    New mappings in which-key

--- lua/config/whichkey.lua
+++ lua/config/whichkey.lua
@@ -9,48 +9,73 @@ vim.keymap.set("n", "<c-t>", ":Telescope<CR>")
 -- Setting up register for 'which key' with keymappings
 local wk = require("which-key")
 wk.add({
-    { "<leader>l", "<cmd>Lazy<cr>", desc = "Lazy" },
-    { "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil file manager" },
-    { "<leader>p", "<cmd>Pounce<cr>", desc = "Pounce" },
-    { "<leader>s", '<cmd> lua require("persistence").load() <cr>', desc = "Restore last session" },
-    { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal toggle" },
-    { "<leader>U", "<cmd>Telescope undo<cr>", desc = "Undo" },
+    --                                 ╭───────────╮
+    --                                 │ Root menu │
+    --                                 ╰───────────╯
+    { "<leader>l", "<cmd>Lazy<cr>", desc = "Lazy - plugin manager" },
+    { "<leader>o", "<cmd>Oil --float<cr>", desc = "Oil - file manager" },
+    { "<leader>p", "<cmd>Pounce<cr>", desc = "Pounce - motion" },
+    { "<leader>s", '<cmd>lua require("persistence").load()<cr>', desc = "Restore last session" },
+    { "<leader>t", "<cmd>ToggleTerm direction=float<cr>", desc = "Terminal" },
     --                                  ╭────────╮
     --                                  │ Buffer │
     --                                  ╰────────╯
     { "<leader>b", group = "Buffer" },
-    { "<leader>bb", "<cmd>bprev<CR>", desc = "Previous" },
-    { "<leader>be", "<cmd>Neotree buffers<CR>", desc = "Neotree buffers" },
+    { "<leader>ba", "<cmd>b#<CR>", desc = "Switch to alternate" },
+    { "<leader>be", "<cmd>Neotree buffers<CR>", desc = "Neotree" },
+    { "<leader>bf", "<cmd>Telescope buffers<cr>", desc = "Find" },
     { "<leader>bl", "<cmd>ls<CR>", desc = "List" },
+    { "<leader>bm", "<cmd>bm<CR>", desc = "Go to next modified" },
+    { "<leader>bn", "<cmd>bnext<CR>", desc = "Previous" },
+    { "<leader>bp", "<cmd>bprevious<CR>", desc = "Previous" },
     --                                   ╭──────╮
     --                                   │ Find │
     --                                   ╰──────╯
     { "<leader>f", group = "Find" },
     { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Files" },
-    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "File" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help" },
     { "<leader>fm", "<cmd>Telescope man_pages<cr>", desc = "man pages" },
-    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Notify messages" },
+    { "<leader>fn", "<cmd>Telescope notify<cr>", desc = "Notifications" },
     { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
-    { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Fuzzy find in buffer" },
-    { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Old Yanks" },
+    { "<leader>fs", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "FZF in buffer" },
+    { "<leader>fy", "<cmd>Telescope neoclip<cr>", desc = "Yanks" },
     { "<leader>fz", "<cmd>Telescope spell_suggest<cr>", desc = "Spelling suggestions" },
     --                                    ╭─────╮
     --                                    │ Git │
     --                                    ╰─────╯
     { "<leader>g", group = "Git" },
-    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Blame toggle" },
-    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Shows previous commits" },
-    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive Git" },
+    { "<leader>gb", "<cmd>GitBlameToggle<cr>", desc = "Blame" },
+    { "<leader>gc", "<cmd>Telescope git_commits<cr>", desc = "Commits" },
+    { "<leader>gg", "<cmd>Neogit<cr>", desc = "Interactive" },
     { "<leader>gs", "<cmd>Telescope git_status<cr>", desc = "Status" },
     --                                 ╭───────────╮
     --                                 │ Interface │
     --                                 ╰───────────╯
     { "<leader>u", group = "Interface" },
-    { "<leader>uC", "<cmd>set colorcolumn=80<CR>", desc = "Colorcolumn at 80" },
-    { "<leader>uh", "<cmd>set list!<CR>", desc = "Hidden Characters Toggle" },
-    { "<leader>ul", "<cmd>set number!<cr>", desc = "Line number toggle" },
-    { "<leader>ur", "<cmd>set relativenumber!<cr>", desc = "Relative line number toggle" },
-    { "<leader>uw", "<cmd>set wrap!<cr>", desc = "Wrap line toggle" },
+    {
+        "<leader>uC",
+        function()
+            if vim.wo.colorcolumn == "" then
+                vim.wo.colorcolumn = "80"
+            else
+                vim.wo.colorcolumn = ""
+            end
+        end,
+        desc = "Colorcolumn",
+    },
+    { "<leader>uh", "<cmd>set list!<CR>", desc = "Hidden Characters" },
+    { "<leader>ul", "<cmd>set number!<cr>", desc = "Line numbers" },
+    { "<leader>ur", "<cmd>set relativenumber!<cr>", desc = "Relative line numbers" },
+    { "<leader>us", "<cmd>noh<cr>", desc = "Hide search results" },
+    { "<leader>uw", "<cmd>set wrap!<cr>", desc = "Wrap lines" },
+    --                                  ╭─────────╮
+    --                                  │ Window  │
+    --                                  ╰─────────╯
+    { "<leader>w", group = "Window" },
+    { "<leader>wc", "<cmd>close<cr>", desc = "Close" },
+    { "<leader>wo", "<cmd>only<cr>", desc = "Close all but not current" },
+    { "<leader>ws", "<cmd>split<cr>", desc = "Split horizontal" },
+    { "<leader>wv", "<cmd>vsplit<cr>", desc = "Split vertical" },
 })
